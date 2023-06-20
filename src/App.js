16c49fefc519623092e31eb99a30314bbfa53e49
irinamihai150 <irinamihai150@gmail.com>
2023-06-20 20:52:06 +0100

    set up project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-
+import React from "react"
+import AddUser from "./components/AddUser"
+import Button from "./components/Button"
+import UserList from "./components/UserList"
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+	return (
+		<div>
+			<AddUser />
+			<Button />
+			<UserList />
+		</div>
+	)
 }
 
-export default App;
+export default App
