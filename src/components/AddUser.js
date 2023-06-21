ca2c77c82fb3d4b22abdade449d6d0c65a50c0e2
irinamihai150 <irinamihai150@gmail.com>
2023-06-21 21:27:34 +0100

    add error modal

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -2,10 +2,12 @@ import React, { useState } from "react"
 import classes from "./AddUser.module.css"
 import Card from "./Card"
 import Button from "./Button"
+import ErrorModal from "./ErrorModal"
 
 const AddUser = (props) => {
 	const [enteredUsername, setEnteredUsername] = useState("")
 	const [enteredAge, setEnteredAge] = useState("")
+
 	const addUserHandler = (event) => {
 		event.preventDefault()
 		if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
@@ -18,34 +20,40 @@ const AddUser = (props) => {
 		setEnteredUsername("")
 		setEnteredAge("")
 	}
+
 	const handleUsernameChange = (event) => {
 		setEnteredUsername(event.target.value)
 	}
+
 	const handleAge = (event) => {
 		setEnteredAge(event.target.value)
 	}
+
 	return (
-		<Card className={classes.input}>
-			<form onSubmit={addUserHandler}>
-				<label htmlFor='username'>Username</label>
-				<input
-					className={classes.input}
-					id='username'
-					type='text'
-					value={enteredUsername}
-					onChange={handleUsernameChange}
-				/>
-				<label htmlFor='age'>Age(years)</label>
-				<input
-					className={classes.input}
-					id='age'
-					type='number'
-					value={enteredAge}
-					onChange={handleAge}
-				/>
-				<Button type='submit'>Add User</Button>
-			</form>
-		</Card>
+		<>
+			<ErrorModal title='An error occurred!' message='Something went wrong' />
+			<Card className={classes.input}>
+				<form onSubmit={addUserHandler}>
+					<label htmlFor='username'>Username</label>
+					<input
+						className={classes.input}
+						id='username'
+						type='text'
+						value={enteredUsername}
+						onChange={handleUsernameChange}
+					/>
+					<label htmlFor='age'>Age (years)</label>
+					<input
+						className={classes.input}
+						id='age'
+						type='number'
+						value={enteredAge}
+						onChange={handleAge}
+					/>
+					<Button type='submit'>Add User</Button>
+				</form>
+			</Card>
+		</>
 	)
 }
 
--- src/components/ErrorModal.js
+++ src/components/ErrorModal.js
@@ -1,11 +1,23 @@
-import React from 'react'
-// import classes from "./ErrorModal.module.css"
-const ErrorModal = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import React from "react"
+import Card from "./Card"
+import classes from "./ErrorModal.module.css"
+const ErrorModal = (props) => {
+	return (
+		<div>
+			<div className={classes.backdrop} />
+			<Card className={classes.modal}>
+				<header className={classes.header}>
+					<h2>{props.title}</h2>
+				</header>
+				<div className={classes.content}>
+					<p>{props.message}</p>
+				</div>
+				<footer className={classes.actions}>
+					<button>Okay</button>
+				</footer>
+			</Card>
+		</div>
+	)
 }
 
 export default ErrorModal
